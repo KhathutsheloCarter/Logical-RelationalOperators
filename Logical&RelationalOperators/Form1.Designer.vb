0fec588f7a715841c08ff6bb80e0c9148879f9cd
fundedtraderSA <fundedtraderSA@DESKTOP-0LEPCTF>
2023-09-06 08:56:44 +0200

    Add project files.

---
+++ Logical&RelationalOperators/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnGrade = New Button()
+        lblScore = New Label()
+        txtScore = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnGrade
+        ' 
+        btnGrade.Location = New Point(326, 186)
+        btnGrade.Name = "btnGrade"
+        btnGrade.Size = New Size(124, 74)
+        btnGrade.TabIndex = 0
+        btnGrade.Text = "Get Grade"
+        btnGrade.UseVisualStyleBackColor = True
+        ' 
+        ' lblScore
+        ' 
+        lblScore.AutoSize = True
+        lblScore.Location = New Point(125, 106)
+        lblScore.Name = "lblScore"
+        lblScore.Size = New Size(199, 20)
+        lblScore.TabIndex = 1
+        lblScore.Text = "Enter the examination Score!"
+        ' 
+        ' txtScore
+        ' 
+        txtScore.Location = New Point(346, 99)
+        txtScore.Name = "txtScore"
+        txtScore.Size = New Size(125, 27)
+        txtScore.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtScore)
+        Controls.Add(lblScore)
+        Controls.Add(btnGrade)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnGrade As Button
+    Friend WithEvents lblScore As Label
+    Friend WithEvents txtScore As TextBox
+End Class
---
+++ Logical&RelationalOperators/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub btnGrade_Click(sender As Object, e As EventArgs) Handles btnGrade.Click
+
+        Dim iScore As Integer
+        iScore = txtScore.Text
+
+
+        If iScore < 0 Or iScore > 100 Then
+            MsgBox("That is not a Valid Score.  Please Enter a score between 0 and 100 ")
+
+            Exit Sub
+
+
+        ElseIf iScore < 50 Then
+            MessageBox.Show("Fail")
+
+
+        ElseIf iScore >= 50 And iScore < 75 Then
+            MsgBox("pass")
+
+
+        ElseIf iScore >= 75 Then
+            MsgBox("Pass Distiction")
+        End If
+
+        MsgBox("All done !")
+
+
+    End Sub
+End Class
